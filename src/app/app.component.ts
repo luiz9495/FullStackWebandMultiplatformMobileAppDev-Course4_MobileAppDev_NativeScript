6c0bcddaa44d81314bf48af279bbbb89257d9fea
Luiz Cunha <luiz9495@gmail.com>
2019-05-05 16:50:05 +0900

    Platform and Connectivity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,53 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { NavigationEnd, Router } from "@angular/router";
 import * as app from "tns-core-modules/application";
 import { RouterExtensions } from "nativescript-angular/router";
 import { DrawerTransitionBase, RadSideDrawer, SlideInOnTopTransition } from "nativescript-ui-sidedrawer";
 import { filter } from "rxjs/operators";
 import { TNSFontIconService } from 'nativescript-ngx-fonticon';
+import { PlatformService } from './services/platform.service';
 
 @Component({
     selector: "ns-app",
     moduleId: module.id,
     templateUrl: "./app.component.html"
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
     private _activatedUrl: string;
     private _sideDrawerTransition: DrawerTransitionBase;
 
-    constructor(private router: Router, private routerExtensions: RouterExtensions,
-              private fonticon: TNSFontIconService) {
+    constructor(private router: Router,
+              private routerExtensions: RouterExtensions,
+              private fonticon: TNSFontIconService,
+              private platformService: PlatformService) {
         // Use the component constructor to inject services.
     }
 
     ngOnInit(): void {
-        this._activatedUrl = "/menu";
-        this._sideDrawerTransition = new SlideInOnTopTransition();
+      this._activatedUrl = "/menu";
+      this._sideDrawerTransition = new SlideInOnTopTransition();
 
-        this.router.events
-        .pipe(filter((event: any) => event instanceof NavigationEnd))
-        .subscribe((event: NavigationEnd) => this._activatedUrl = event.urlAfterRedirects);
+      this.router.events
+      .pipe(filter((event: any) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => this._activatedUrl = event.urlAfterRedirects);
+
+      this.platformService.printPlatformInfo();
+      this.platformService.startMonitoringNetwork()
+      .subscribe((message: string) => {
+        console.log(message);
+      });
+    }
+
+    ngOnDestroy() {
+      this.platformService.stopMonitoringNetwork();
     }
 
     get sideDrawerTransition(): DrawerTransitionBase {
-        return this._sideDrawerTransition;
+      return this._sideDrawerTransition;
     }
 
     isComponentSelected(url: string): boolean {
-        return this._activatedUrl === url;
+      return this._activatedUrl === url;
     }
 
     onNavItemTap(navItemRoute: string): void {
